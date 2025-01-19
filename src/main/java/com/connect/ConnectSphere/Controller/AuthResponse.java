fe82691942edda76b5de89a189baf6a87b52b8b9
AnujYadav2003 <120973638+AnujYadav2003@users.noreply.github.com>
2025-01-19 12:36:35 +0530

    Update AuthResponse.java

--- src/main/java/com/connect/ConnectSphere/Controller/AuthResponse.java
+++ src/main/java/com/connect/ConnectSphere/Controller/AuthResponse.java
@@ -17,35 +17,3 @@ public class AuthResponse {
 
 }
 
-//package com.connect.ConnectSphere.Controller;
-//
-//public class AuthResponse {
-//    private String token;
-//    private String message;
-//
-//    // Default constructor
-//    public AuthResponse() {}
-//
-//    // Parameterized constructor
-//    public AuthResponse(String token, String message) {
-//        this.token = token;
-//        this.message = message;
-//    }
-//
-//    // Getters and Setters
-//    public String getToken() {
-//        return token;
-//    }
-//
-//    public void setToken(String token) {
-//        this.token = token;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
-//}
